25cd2d25e75c32ca90f9c728696b472c1be9a722
M. Machrush Aliy Sirojjam Mushlich <jampirojam@gmail.com>
2022-03-26 11:26:05 +0700

    Update Jawaban 03.java

--- Jawaban 03.java
+++ Jawaban 03.java
@@ -1,9 +1,8 @@
 public class Main {
   public static void main(String[] args) {
-    for (int i = 0; i < 5; i++) {
-    	System.out.format(" %d ", i+1);
-    	for (int j = 1; j <= i; j++) {
-       	System.out.format(" %d ", i+1+j);
+    for (int i = 1; i < 6; i++) {
+    	for (int j = 0; j < i; j++) {
+       	System.out.format(" %d ", i+j);
       }
       System.out.println(" ");
     }
